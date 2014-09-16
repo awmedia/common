524caeba0c05d4bcac25e28600bb318dc8097cd4
Jerry <email@sjerry.nl>
2014-09-16 16:44:27 +0200

    first commit

---
+++ lib/Aw/Common/Spl/Iterators/CsvIterator.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace Aw\Common\Spl\Iterators;
+
+use \Iterator,
+    \Exception;
+
+/**
+ * CSV Iterator
+ * @author Jon LaBelle
+ * @author Jerry Sietsma
+ */
+class CsvIterator implements Iterator
+{
+    /**
+     * Must be greater than the longest line (in characters) to be found in
+     * the CSV file (allowing for trailing line-end characters).
+     *
+     * @var int
+     */
+    const ROW_LENGTH = 2048;
+
+    /**
+     * Resource file pointer
+     */
+    private $_filePointer;
+
+    /**
+     * Represents current element in iteration
+     *
+     * @var int
+     */
+    private $_currentElement;
+
+    /**
+     * Cumalitve row count of CSV data
+     *
+     * @var int
+     */
+    private $_rowCounter;
+
+    /**
+     * CSV column delimeter
+     *
+     * @var string
+     */
+    private $_delimiter;
+    
+    private $_enclosure;
+    
+    private $_escape;
+    
+    private $_hasHeaderWithColumns;
+    
+    private $_ignoreHeaderWithColumns;
+    
+    private $_useHeaderColumnsAsIndex;
+    
+    private $_ignoreEmptyRows;
+
+    /**
+     * Create an instance of the CsvIterator class.
+     *
+     * @param string $file The CSV file path or a file handle.
+     * @param string $delimiter The default delimeter is a single comma (,)
+     */
+    public function __construct($file, $delimiter = ',', $enclosure = '"', $escape = '\\')
+    {
+        $this->_initializedWithHandle = get_resource_type($file) !== false;
+        $this->_filePointer =  $this->_initializedWithHandle ? $file : fopen($file, 'rt');
+        $this->_delimiter = $delimiter;
+        $this->_enclosure = $enclosure;
+        $this->_escape = $escape;
+        
+        $this->_hasHeaderWithColumns = true;
+        $this->_ignoreHeaderWithColumns = true;
+        $this->_useHeaderColumnsAsIndex = true;
+        $this->_ignoreEmptyRows = true;
+        
+        $this->_columns = null;
+        
+        // init: set columns if available
+        $this->rewind();
+    }
+
+    /*
+     * @see Iterator::rewind()
+     */
+    public function rewind()
+    {
+        $this->_rowCounter = 0;
+        rewind($this->_filePointer);
+        
+        if ($this->_hasHeaderWithColumns === true)
+        {
+            $this->_columns = $this->current();
+            $this->_columnCount = count($this->_columns);
+            
+            if ($this->_ignoreHeaderWithColumns === true)
+            {
+                $this->next();
+            }
+        }
+    }
+
+    /*
+     * @see Iterator::current()
+     */
+    public function current()
+    {
+        $data = fgetcsv($this->_filePointer, self::ROW_LENGTH, $this->_delimiter, $this->_enclosure, $this->_escape);
+        
+        if ($this->_ignoreEmptyRows)
+        {
+            $dataTmp = $data;
+            $dataTmp = array_filter($dataTmp);
+            
+            if (empty($dataTmp))
+            {
+                return $this->current();
+            }
+        }
+        
+        $this->_currentElement = $data;
+        
+        if ($this->_useHeaderColumnsAsIndex && ($this->_hasHeaderWithColumns && $this->_rowCounter > 0))
+        {
+              $this->_currentElement = @array_combine($this->_columns, $data);
+              
+              if ($this->_currentElement === false)
+              {
+                  if (count($data) !== $this->_columnCount)
+                  {
+                      throw new Exception('The number of headers don\'t match the number of columns');
+                  }
+              }
+        }
+        
+        $this->_rowCounter ++;
+
+        return $this->_currentElement;
+    }
+
+    /*
+     * @see Iterator::key()
+     */
+    public function key()
+    {
+        return $this->_rowCounter;
+    }
+
+    /*
+     * @see Iterator::next()
+     */
+    public function next()
+    {
+        return ! feof($this->_filePointer);
+    }
+
+    /*
+     * @see Iterator::valid()
+     */
+    public function valid()
+    {
+        if (!$this->next())
+        {
+            if (!$this->_initializedWithHandle)
+            {
+                fclose($this->_filePointer);
+            }
+            
+            return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Public methods
+     */
+    
+    public function setHasHeaderWithColumns($hasHeaderWithColumns)
+    {
+        $this->_hasHeaderWithColumns = $hasHeaderWithColumns;
+        $this->rewind();
+        return $this;
+    }
+    
+    public function setIgnoreHeaderWithColumns($ignore)
+    {
+        $this->_ignoreHeaderWithColumns = $ignore;
+        $this->rewind();
+        return $this;
+    }
+    
+    public function setUseHeaderColumnsAsIndex($useHeaderColumnsAsIndex)
+    {
+        $this->_useHeaderColumnsAsIndex = (bool) $useHeaderColumnsAsIndex;
+        $this->rewind();
+        return $this;
+    }
+    
+    public function setIgnoreEmptyRows($ignore)
+    {
+        $this->ignoreEmptyRows = $ignore;
+        return $this;
+    }
+    
+    public function columns()
+    {
+        return $this->_columns;
+    }
+}
---
+++ lib/Aw/Common/Utils/ArrayUtils.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Aw\Common\Utils;
+
+/**
+ * ArrayUtils
+ * @author Jerry Sietsma
+ */
+class ArrayUtils
+{
+    
+    /**
+     * Tests if an array is associative or or not.
+     * @param   array   array to check
+     * @return  boolean
+     */
+    public static function isAssoc(array $array)
+    {
+        $keys = array_keys($array);
+        return array_keys($keys) !== $keys;
+    }
+
+    /**
+     * Gets a value from an array using a dot separated path.
+     *     // Get the value of $array['foo']['bar']
+     *     $value = Arr::path($array, 'foo.bar');
+     * @param   array   array to search
+     * @param   string  key path, dot separated
+     * @param   mixed   default value if the path is not set
+     * @return  mixed
+     */
+    public static function path($array, $path, $default = NULL)
+    {
+        $keys = explode('.', $path);
+
+        do
+        {
+            $key = array_shift($keys);
+
+            if (ctype_digit($key))
+            {
+                $key = (int) $key;
+            }
+
+            if (isset($array[$key]))
+            {
+                if ($keys)
+                {
+                    if (is_array($array[$key]))
+                    {
+                        $array = $array[$key];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    return $array[$key];
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        while ($keys);
+
+        return $default;
+    }
+
+    /**
+     * Retreive a single key from an array. If the key does not exist in the
+     * array, the default value will be returned instead.
+     * @param   array   array to extract from
+     * @param   string  key name
+     * @param   mixed   default value
+     * @param    string    the type to cast the value to (optional). Not applicable for default value.
+     * @return  mixed
+     */
+    public static function get(array $array = null, $key, $default = null, $castToType = null)
+    {
+        if (isset($array[$key]))
+        {
+            $value = $array[$key];
+            
+            if ($castToType !== null)
+            {
+                settype($value, $castToType);
+            }
+            
+            return $value;
+        }
+        
+        return $default;
+    }
+
+    /**
+     * Retrieves multiple keys from an array. If the key does not exist in the
+     * array, the default value will be added instead.
+     *
+     * @param   array   array to extract keys from
+     * @param   array   list of key names
+     * @param   mixed   default value
+     * @param    boolean    true to ignore keys that are not set instead of using default
+     * @return  array
+     */
+    public static function extract(array $array, array $keys, $default = null, $ignoreEmpty = false)
+    {
+        $found = array();
+        foreach ($keys as $key)
+        {
+            $isset = isset($array[$key]);
+            
+            if (!$isset && $ignoreEmpty === true)
+            {
+                continue;
+            }
+            
+            $result = $isset ? $array[$key] : $default;
+        }
+
+        return $found;
+    }
+
+    /**
+     * Merges one or more arrays recursively and preserves all keys.
+     * Note that this does not work the same the PHP function array_merge_recursive()!
+     * @param   array  initial array
+     * @param   array  array to merge
+     * @param   array  ...
+     * @return  array
+     */
+    public static function merge(array $a1)
+    {
+        $result = array();
+        for ($i = 0, $total = func_num_args(); $i < $total; $i++)
+        {
+            foreach (func_get_arg($i) as $key => $val)
+            {
+                if (isset($result[$key]))
+                {
+                    if (is_array($val))
+                    {
+                        $result[$key] = self::merge($result[$key], $val);
+                    }
+                    elseif (is_int($key))
+                    {
+                        array_push($result, $val);
+                    }
+                    else
+                    {
+                        $result[$key] = $val;
+                    }
+                }
+                else
+                {
+                    $result[$key] = $val;
+                }
+            }
+        }
+
+        return $result;
+    }
+}
